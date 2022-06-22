ebd4754c34b116dcb7bd5e3b4e858a42149f8c1f
l33t-c0d3r-66 <aliraza.arain.12530@gmail.com>
2022-06-22 13:54:20 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+        <h3>0 Birthdays Today</h3>
+        <List />
+        <button onClick={() => console.log("Clicked")}>Clear All</button>
+      </section>
+    </main>
   );
 }
-
 export default App;
